2a1ad3eb562f441198f98e5e31d3646679a29c60
DeolaDev <deoladeveloper@gmail.com>
2023-03-07 20:41:30 -0500

    updated contact.js

--- src/pages/thankyou.js
+++ src/pages/thankyou.js
@@ -1,9 +1,5 @@
 import React from "react";
 
-export default function Contact() {
-  return (
-    <div>
-      <p>Thank you for your submission.</p>
-    </div>
-  );
-}
+<div>
+  <p>Thank you for your submission.</p>
+</div>;
